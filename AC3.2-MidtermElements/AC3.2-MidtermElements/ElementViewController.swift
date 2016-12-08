609f94120be2a0df61e9db9d4fd7a3513f610e5b
Margaret E. Ikeda <margaretei@gmail.com>
2016-12-08 16:15:45 -0500

    I <3 Scandium

--- AC3.2-MidtermElements/AC3.2-MidtermElements/ElementViewController.swift
+++ AC3.2-MidtermElements/AC3.2-MidtermElements/ElementViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
-
+var my_name = "Margaret Ikeda"
+var favorite_element = "Scandium"
 class ElementViewController: UIViewController {
     var catchTheElements: Element!
+    var postEndpoint = "https://api.fieldbook.com/v1/58488d40b3e2ba03002df662/favorites"
+    //I know this is how it should be, but I'm having property intializer problems. So my favorite element will always be Scandium :-(
+    //var postDict = [my_name : catchTheElements.name]
+    var postDict = [my_name : favorite_element]
     @IBOutlet weak var elementLabel: UILabel!
     @IBOutlet weak var elementImageView: UIImageView!
-    
+    @IBAction func favoritesButton(_ sender: UIButton) {
+    }
+    @IBAction func postRequest (_ sender: UIButton) {
+        APIRequestManager.manager.postRequest(endPoint: postEndpoint, data: postDict)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = catchTheElements.name
         elementLabel.text = "Symbol: \(catchTheElements.symbol)\nNumber: \(catchTheElements.number)\nWeight: \(catchTheElements.weight)\nBoiling Point: \(catchTheElements.boilingPoint) degrees Celsius\nMelting Point: \(catchTheElements.meltingPoint) degrees Celsius"
+        
+        
+        
         APIRequestManager.manager.getData(endPoint: (catchTheElements.detailImage)) { (data: Data?) in
             guard let validData = data else { return }
             DispatchQueue.main.async {
